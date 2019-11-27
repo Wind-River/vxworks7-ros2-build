22801d2cbb49f5811492531f08de7348652acd2f
Rob Woolley <rob.woolley@windriver.com>
2019-11-27 21:14:46 +0100

    rpath fixed

--- buildspecs/cmake/Platform/VxWorks.cmake
+++ buildspecs/cmake/Platform/VxWorks.cmake
@@ -33,6 +33,20 @@ SET(CMAKE_EXECUTABLE_SUFFIX ".vxe")
 # include(Platform/GNU)
 # See https://index.ros.org/doc/ros2/Tutorials/Cross-compilation/#cmake-toolchain-file
 
+# SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+SET(CMAKE_SKIP_RPATH  TRUE)
+
+set(CMAKE_DL_LIBS "dl")
+set(CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG "-Wl,-rpath-link,")
+set(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-Wl,-soname,")
+set(CMAKE_EXE_EXPORTS_C_FLAG "-Wl,--export-dynamic")
+set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "-Wl,-rpath,")
+set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")
+
+set(CMAKE_SHARED_LIBRARY_C_FLAGS "-fPIC")
+set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-shared")
+
+
 set(CMAKE_SYSROOT $ENV{WIND_SDK_TOOLKIT})
 
 # set(CMAKE_C_COMPILER $ENV{CC})
