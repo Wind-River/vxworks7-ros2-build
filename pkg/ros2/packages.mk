2358968a2ee2a3d758c942561b967d32e09930d8
Andrei Kholodnyi <andrei.kholodnyi@gmail.com>
2019-12-05 00:27:46 +0100

    patch geometry2 package

--- pkg/ros2/packages.mk
+++ pkg/ros2/packages.mk
@@ -27,6 +27,7 @@ ROS2_PATCH_DIRS=eProsima/Fast-CDR \
                 ros2/console_bridge_vendor \
                 ros2/demos \
                 ros2/examples \
+                ros2/geometry2 \
                 ros2/rcl \
                 ros2/rclcpp \
                 ros2/rcutils \
