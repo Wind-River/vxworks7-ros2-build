7ed20ddd3482f07e54b5ac3160be350c683db49b
Andrei Kholodnyi <andrei.kholodnyi@gmail.com>
2020-05-06 13:16:51 +0200

    add ROS2_PYTHON_TOOLS

--- pkg/ros2/packages.mk
+++ pkg/ros2/packages.mk
@@ -78,7 +78,12 @@ ROS2_EXAMPLES=examples_rclcpp_minimal_action_client \
 	examples_rclcpp_minimal_service \
 	examples_rclcpp_minimal_timer
 
+ROS2_PYTHON_TOOLS=ros2action ros2component ros2msg ros2node ros2pkg \
+	ros2service ros2topic ros2cli ros2lifecycle ros2multicast ros2param \
+	ros2run ros2srv
+
 PKG_PKGS_UP_TO=$(ROS2_EXAMPLES) \
+	$(ROS2_PYTHON_TOOLS) \
 	pendulum_control
 
 #	dummy_map_server \
