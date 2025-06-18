dcd002a9418256a57b8a307062b6caec5554cca2
Andrei Kholodnyi <andrei.kholodnyi@windriver.com>
2025-06-18 15:22:04 +0200

    update flock test

--- pkg/unixextra/files/tests/flock_test.c
+++ pkg/unixextra/files/tests/flock_test.c
@@ -1,6 +1,33 @@
 /* flock_test - test program for flock
- *
- */
+
+Usage: ./flock_test <filename> <sn>
+       ./flock_test <sn>
+       ./flock_test
+
+[vxWorks *]# echo "" > /tmp/stamp
+
+Example usages:
+"sn" — shared, non-blocking lock (LOCK_SH | LOCK_NB)
+"s"  — shared, blocking lock (LOCK_SH)
+"xn" — exclusive, non-blocking (LOCK_EX | LOCK_NB)
+"x"  — exclusive, blocking (LOCK_EX)
+
+# exclusive, blocking example
+
+Terminal 1:
+[vxWorks *]# /bd0a/flock_test
+Launching process '/bd0a/flock_test' ...
+Process '/bd0a/flock_test' (process Id = 0xffff8000003fe580) launched.
+Press <RETURN> to acquire an exclusive (blocking) lock on /tmp/stamp ... 
+done
+
+Terminal 2:
+[vxWorks *]# /bd0a/flock_test
+Launching process '/bd0a/flock_test' ...
+Process '/bd0a/flock_test' (process Id = 0xffff8000003fb8d0) launched.
+Press <RETURN> to acquire an exclusive (blocking) lock on /tmp/stamp ... 
+
+*/
 
 #include <sys/file.h>
 #include <fcntl.h>
@@ -18,25 +45,37 @@ int main (
     )
 {
     int fd, lock = LOCK_EX;
-	const char *fname = "/tmp/stamp";
+    char *fname = "/tmp/stamp";
 
-	/* parse input parameters
-	 * flock_test [filename] [lock_opt:sn]
-	 */
+    /* parse input parameters
+     * flock_test [filename] [lock_opt:sn] */
+
     if (argc > 1)
+    {
         if (argv[1][0] == 's' || argv[1][1] == 'n')
         {
             lock = (argv[1][0] == 's') ? LOCK_SH : LOCK_EX;
             lock |= (argv[1][1] == 'n') ? LOCK_NB : 0;
-            fd = open (fname, O_RDWR);
         }
         else
-            fd = open (argv[1], O_RDWR);
-    else
-        fd = open (fname, O_RDWR);
+        {
+            fname = argv[1];
+            if (argv[2][0] == 's' || argv[2][1] == 'n')
+            {
+                lock = (argv[2][0] == 's') ? LOCK_SH : LOCK_EX;
+                lock |= (argv[2][1] == 'n') ? LOCK_NB : 0;
+            }
+        }
+    }
 
+    fd = open (fname, O_RDWR);
     if (fd == -1 )
-        { printf ("open %s failed with %d\n", fname, errno); exit (-1);}
+    {
+        printf("open %s failed with %d (%s)\n", fname, errno, strerror(errno));
+        if(errno == ENOENT)
+            printf("Create it first: [vxWorks *]# echo > %s\n", fname);
+        exit (-1);
+    }
 
     if (argc > 2)
         if (argv[2][0] == 's' || argv[2][1] == 'n')
@@ -46,7 +85,7 @@ int main (
         }
 
     printf ("Press <RETURN> to acquire %s %s lock on %s ... ",
-        (lock & LOCK_EX)?"an exclusive":"a shared", (lock & LOCK_NB)?"(non-blocking)":"", fname);
+        (lock & LOCK_EX)?"an exclusive":"a shared", (lock & LOCK_NB)?"(non-blocking)":"(blocking)", fname);
     fflush(stdout);
     getchar();
 
